5809de7cdb10e8ec46e36d5d71e311f90af3a433
Chz216 <joseluis.romero@alumno.uttehuacan.edu.mx>
2017-07-23 17:28:04 -0500

    imagenes index

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 include_once 'logica/funciones.php';
 $fun = new funciones();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,74 +14,58 @@ $fun = new funciones();
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/style.css">
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.css">
-  <link rel="stylesheet" href="newstyle.css">
+  <link rel="stylesheet" href="Style.css">
   <title>CDMU</title>
 </head>
-<body  >
 
+<body>
   <?php $fun->menu(); ?>
+  <img src="Imagenes/anunciate2.jpg" class="img-responsive" width="1800px" >
 
-
-
-  <img src="Imagenes/anunciate2.jpg"    class="img-responsive" alt="Hola" width="1800px" >
-
-</div>
   <!-- CUERPO -->
   <div class="container">
+    <hr>
+      <div class="container-fluid" >
 
-
-    <br>
-
-
-      <div class="col-xs-12 col-sm-12 col-md-12" >
       <div id="carousel1" class="carousel slide" data-ride="carousel">
+
         <ol class="carousel-indicators">
           <li data-target="#carousel1" data-slide-to="0" class="active"></li>
           <li data-target="#carousel1" data-slide-to="1"></li>
           <li data-target="#carousel1" data-slide-to="2"></li>
-          <li data-target="#carousel1" data-slide-to="3"></li>
         </ol>
 
-
-        <div class="container ">
-        <div class="carousel-inner" role="listbox" >
-          <div class="item active" >
-            <img src="Imagenes/empresas/coppel1.jpg"  style="width:800px; height:250px">
-
-            </div>
-
-            <div class="item " >
-              <img src="Imagenes/empresas/suburbia1.jpg"  style="width:1600px; height:250px">
+        <div class="carousel-inner">
+            <div class="item active" >
+              <img src="Imagenes/empresas/suburbia1.jpg"  style="width:600px; height:150px">
             </div>
 
               <div class="item">
-                <img src="Imagenes/empresas/walmart1.png"  style="width:1600px; height:250px">
+                <img src="Imagenes/empresas/walmart1.png"  style="width:600px; height:150px">
+              </div>
 
-                </div>
+              <div class="item">
+                <img src="Imagenes/empresas/woolworth1.jpg"  style="width:600px; height:150px">
+              </div>
+              </div>
+              </div>
+              </div>
 
-                <div class="item">
-                  <img src="Imagenes/empresas/woolworth1.jpg"  style="width:1600px; height:250px">
-
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-                  </div>
+              <hr>
 
                 <div class="container">
-                <br>
                   <div class="row featurette">
                     <div class="col-md-7">
-                      <h2 class="featurette-heading">Servicios </h2>
+                      <h2 class="featurette-heading">Servicios</h2>
                       <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                     </div>
+
                     <div class="col-md-5">
-                      <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDUwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzUwMHg1MDAvYXV0bwpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxlIHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1ZDZkMjY2NjAzIHRleHQgeyBmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MjVwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTVkNmQyNjY2MDMiPjxyZWN0IHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIiBmaWxsPSIjRUVFRUVFIi8+PGc+PHRleHQgeD0iMTg1LjEyNSIgeT0iMjYxLjEiPjUwMHg1MDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+                      <img src="img/serv1.jpg" style="width:350px; height:250px">
                     </div>
                   </div>
 
-                  <hr class="featurette-divider">
+                  <hr>
 
                   <div class="row featurette">
                     <div class="col-md-7 col-md-push-5">
@@ -87,11 +73,11 @@ $fun = new funciones();
                       <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                     </div>
                     <div class="col-md-5 col-md-pull-7">
-                      <img class="featurette-image img-responsive center-block" data-src="holder.js/350x350/auto" alt="500x500" src="img/cursos1.jpg" data-holder-rendered="true">
+                      <img src="img/curs1.jpg" style="width:350px; height:250px">
                     </div>
                   </div>
 
-                  <hr class="featurette-divider">
+                  <hr>
 
                   <div class="row featurette">
                     <div class="col-md-7">
@@ -99,22 +85,12 @@ $fun = new funciones();
                       <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                     </div>
                     <div class="col-md-5">
-                      <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="350x350" src="img/cursos.jpg"  data-holder-rendered="true">
+                      <img src="img/cursos.jpg" style="width:350px; height:250px">
                     </div>
+                  </div>  
                   </div>
-                  <hr class="featurette-divider">
-                  </div>
-
-
-
-
-
-
-
-
-
-          <hr>
         </div>
+        <hr>
   <?php $fun->carfo();?>
         <script src="bower_components/jquery/dist/jquery.js"></script>
   <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
--- logica/funciones.php
+++ logica/funciones.php
@@ -1,6 +1,6 @@
 <?php
-include_once '/database.php';
 
+include_once 'logica/database.php';
 
 class funciones {
 
@@ -40,7 +40,7 @@ function __construct(){
         </div><!-- /.container-fluid -->
       </nav>
 
-<hr />
+<hr/>
           </div>
         </div>
       </div>
@@ -51,7 +51,7 @@ function __construct(){
     public function footer() {
         echo'<footer>
 
-      <div class="footer" style="background: #036449">
+      <div class="footer" style="background:#036449">
         <div class="container">
           <div class="row">
           <br>
@@ -63,7 +63,6 @@ function __construct(){
             </div>
             <div class="text-center col-xs-12 col-sm-12 col-md-4 " style="color:white" >
               <ul class="list-unstyled text-center list-inline" style= "font-size:30px; >
-
                 <li class="icono"><a href="#"><i class="fa fa-facebook-square" style="color: white"></i></a></li>
                 <li class="icono"><a href="#"><i class="fa fa-twitter-square" style="color: white"></i></a></li>
                 <li class="icono"><a href="#"><i class="fa fa-linkedin-square" style="color: white"></i></a></li>
@@ -80,13 +79,16 @@ function __construct(){
 
     public function carfo(){
         echo'<div class="container">
-            <div id="carousel-products" class="carousel slide container" data-ride="carousel">
+
+            <div id="carousel-products" class="carousel slide" data-ride="carousel">
                 <!-- indicatodors -->
+
                 <ol class="carousel-indicators">
                     <li data-target="#carousel-products" data-slide-to="0" class="active"></li>
                     <li data-target="#carousel-products" data-slide-to="1"></li>
                     <li data-target="#carousel-products" data-slide-to="2"></li>
                 </ol>
+
                 <div class="carousel-inner">
                     <div class="item active">
                         <img src="img/afiliarse.jpg" alt="" style="width:1000px; height:250px">
