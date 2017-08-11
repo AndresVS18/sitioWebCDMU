ee8eaa909e736a24ebaaffde032c95a1db9cf122
CarlosVJ <cmvjimenez@hotmail.com>
2017-08-11 00:28:40 -0500

    fin

--- logica/pagina_web.sql
+++ logica/pagina_web.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.1.4
+-- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-08-2017 a las 00:51:27
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 5.6.30
+-- Tiempo de generación: 11-08-2017 a las 07:08:25
+-- Versión del servidor: 5.6.15-log
+-- Versión de PHP: 5.5.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `pagina_web`
@@ -26,11 +26,12 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `administrador`
 --
 
-CREATE TABLE `administrador` (
-  `Id_administrador` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `Id_administrador` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre_usu` varchar(50) NOT NULL,
-  `Contraseña` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Contraseña` varchar(30) NOT NULL,
+  PRIMARY KEY (`Id_administrador`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `administrador`
@@ -47,8 +48,8 @@ INSERT INTO `administrador` (`Id_administrador`, `Nombre_usu`, `Contraseña`) VA
 -- Estructura de tabla para la tabla `afiliados`
 --
 
-CREATE TABLE `afiliados` (
-  `Id_afiliado` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `afiliados` (
+  `Id_afiliado` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(30) NOT NULL,
   `Apellidos` varchar(30) NOT NULL,
   `Email` varchar(30) NOT NULL,
@@ -56,8 +57,9 @@ CREATE TABLE `afiliados` (
   `Telefono` double NOT NULL,
   `Nombre_usuario` varchar(50) NOT NULL,
   `passwors` varchar(50) NOT NULL,
-  `activo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `activo` int(11) NOT NULL,
+  PRIMARY KEY (`Id_afiliado`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `afiliados`
@@ -74,15 +76,17 @@ INSERT INTO `afiliados` (`Id_afiliado`, `Nombre`, `Apellidos`, `Email`, `Direcci
 -- Estructura de tabla para la tabla `aspirantes`
 --
 
-CREATE TABLE `aspirantes` (
-  `Id_aspi` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `aspirantes` (
+  `Id_aspi` int(11) NOT NULL AUTO_INCREMENT,
   `Id_curs` int(11) NOT NULL,
   `Nombre_aspi` varchar(30) NOT NULL,
   `Apellidos` varchar(30) NOT NULL,
   `Telefono` int(11) NOT NULL,
   `Direccion` varchar(30) NOT NULL,
-  `Email` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Email` varchar(30) NOT NULL,
+  PRIMARY KEY (`Id_aspi`),
+  KEY `Id_curs` (`Id_curs`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Volcado de datos para la tabla `aspirantes`
@@ -100,14 +104,16 @@ INSERT INTO `aspirantes` (`Id_aspi`, `Id_curs`, `Nombre_aspi`, `Apellidos`, `Tel
 -- Estructura de tabla para la tabla `bolsa_formulario`
 --
 
-CREATE TABLE `bolsa_formulario` (
-  `Id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `bolsa_formulario` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Id_bolsa` int(11) NOT NULL,
   `Nombre` int(11) NOT NULL,
   `Correo` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `Telefono` bigint(15) NOT NULL,
-  `Mensaje` text COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+  `Mensaje` text COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `id_bolsa` (`Id_bolsa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -115,15 +121,17 @@ CREATE TABLE `bolsa_formulario` (
 -- Estructura de tabla para la tabla `bolsa_trabajo`
 --
 
-CREATE TABLE `bolsa_trabajo` (
-  `Id_bolsa` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `bolsa_trabajo` (
+  `Id_bolsa` int(11) NOT NULL AUTO_INCREMENT,
   `Id_afiliado` int(11) NOT NULL,
   `Nombre_vacante` varchar(100) NOT NULL,
   `Contenido` varchar(100) NOT NULL,
   `Telefono` double NOT NULL,
   `Direccion` varchar(100) NOT NULL,
-  `Fecha_limite` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Fecha_limite` date NOT NULL,
+  PRIMARY KEY (`Id_bolsa`),
+  KEY `Id_afiliado` (`Id_afiliado`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
 --
 -- Volcado de datos para la tabla `bolsa_trabajo`
@@ -132,7 +140,7 @@ CREATE TABLE `bolsa_trabajo` (
 INSERT INTO `bolsa_trabajo` (`Id_bolsa`, `Id_afiliado`, `Nombre_vacante`, `Contenido`, `Telefono`, `Direccion`, `Fecha_limite`) VALUES
 (1, 1, 'Diseñador', 'Se solicita empleado', 9898877676, 'AV LOS 4 CAMINOS', '2017-08-12'),
 (2, 2, 'Encargado', 'Se solicita encargado para nave textil en diseño de playera.', 5587623456, 'AV INDEPENDENCIA 123', '2017-08-12'),
-(3, 1, 'Mecanico', 'Se solicita mecánico para el área de maquinas textiles.', 9898877676, 'AV LOS 4 CAMINOS', '2017-08-12'),
+(3, 1, 'Mecánico', 'Se solicita mecánico para el área de máquinas textiles.', 9898877676, 'AV LOS 4 CAMINOS', '2017-08-12'),
 (4, 2, 'Auxiliar de almacén', 'Se solicita auxiliar de almacén para el área de oficina. ', 5587623456, 'AV INDEPENDENCIA 123', '2017-08-12'),
 (5, 3, 'Overlista', 'Se solicita costurero para el área de maquina over.', 9812345875, '1 ORIENTE 456', '2017-08-12');
 
@@ -142,23 +150,25 @@ INSERT INTO `bolsa_trabajo` (`Id_bolsa`, `Id_afiliado`, `Nombre_vacante`, `Conte
 -- Estructura de tabla para la tabla `cursos`
 --
 
-CREATE TABLE `cursos` (
-  `Id_curs` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `Id_curs` int(11) NOT NULL AUTO_INCREMENT,
   `Id_admin` int(11) NOT NULL,
   `Nombre_curs` varchar(30) NOT NULL,
   `Tipo_curso` text NOT NULL,
   `Fecha_curso` date NOT NULL,
-  `imagen` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `imagen` varchar(50) NOT NULL,
+  PRIMARY KEY (`Id_curs`),
+  KEY `Id_admin` (`Id_admin`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Volcado de datos para la tabla `cursos`
 --
 
 INSERT INTO `cursos` (`Id_curs`, `Id_admin`, `Nombre_curs`, `Tipo_curso`, `Fecha_curso`, `imagen`) VALUES
-(1, 1, 'Curso de Bordado', 'En este  curso de bordado básico vamos a aprender a realizar puntadas esenciales en el bordado, mediante pequeños vídeos con explicaciones escritas y sin audio, muy faciles de seguir.\nLas puntadas que vamos a ver en este curso son:\nEl cordoncillo.\nLa cadeneta.\nRelleno de punto de arroz.\nRelleno de cordoncillo.', '2017-08-01', 'slide-2.jpg'),
+(1, 1, 'Curso de Bordado', 'En este  curso de bordado básico vamos a aprender a realizar puntadas esenciales en el bordado, mediante pequeños vídeos con explicaciones escritas y sin audio, muy fáciles de seguir.\r\nLas puntadas que vamos a ver en este curso son:\r\nEl cordoncillo.\r\nLa cadeneta.\r\nRelleno de punto de arroz.\r\nRelleno de cordoncillo.', '2017-08-01', 'slide-2.jpg'),
 (2, 2, 'Curso de Serigrafía ', 'Este sistema, por sus especiales características, permite imprimir sobre cualquier soporte: blanco, transparente o de color, grueso o fino, áspero, rugoso o suave, de forma regular o irregular, mate, semimate o brillante, pequeño o grande, de forma plana o cilíndrica, y se pueden emplear todo tipo de tintas, previamente formuladas de acuerdo con los materiales en los que se va a imprimir, con diferentes gruesos de capa por depósito de tinta, calidades opacas, transparentes, mates, semimates, brillantes, fluorescentes, reflectantes, barnices y lacas, vinílicas, acrílicas, gliceroftálicas, catalépticas o de los componentes, al agua, etc. Se aplica sobre cualquier tipo de material: papel, cartulina, cartón, cuero, corcho, metal, madera, plástico, cristal, telas orgánicas o sintéticas, fieltro, cerámica, etc., y sin ninguna limitación en el número de colores planos o tramados, pudiendo hacerse la impresión manual o por medio de máquinas.', '2017-08-07', 'slide-3.jpg'),
-(5, 3, 'Base de Datos', 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo \"Contenido aquí, contenido aquí\". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de \"Lorem Ipsum\" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).', '2017-07-12', 'intro-pic.jpg');
+(5, 3, 'Base de Datos', 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).', '2017-07-12', 'intro-pic.jpg');
 
 -- --------------------------------------------------------
 
@@ -166,15 +176,17 @@ INSERT INTO `cursos` (`Id_curs`, `Id_admin`, `Nombre_curs`, `Tipo_curso`, `Fecha
 -- Estructura de tabla para la tabla `maquila`
 --
 
-CREATE TABLE `maquila` (
-  `Id_maqui` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `maquila` (
+  `Id_maqui` int(11) NOT NULL AUTO_INCREMENT,
   `Id_afiliado` int(11) NOT NULL,
   `Nombre_maqui` varchar(30) NOT NULL,
   `Direccion` varchar(30) NOT NULL,
   `Telefono` double NOT NULL,
   `Email` varchar(30) NOT NULL,
-  `Descripcion` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Descripcion` varchar(30) NOT NULL,
+  PRIMARY KEY (`Id_maqui`),
+  KEY `Id_afiliado` (`Id_afiliado`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Volcado de datos para la tabla `maquila`
@@ -193,13 +205,15 @@ INSERT INTO `maquila` (`Id_maqui`, `Id_afiliado`, `Nombre_maqui`, `Direccion`, `
 -- Estructura de tabla para la tabla `noticias`
 --
 
-CREATE TABLE `noticias` (
-  `Id_not` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `noticias` (
+  `Id_not` int(11) NOT NULL AUTO_INCREMENT,
   `Id_admin` int(11) NOT NULL,
   `Titulo` varchar(100) NOT NULL,
   `Fecha` date NOT NULL,
-  `Contenido` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Contenido` text NOT NULL,
+  PRIMARY KEY (`Id_not`),
+  KEY `Id_admin` (`Id_admin`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `noticias`
@@ -216,13 +230,15 @@ INSERT INTO `noticias` (`Id_not`, `Id_admin`, `Titulo`, `Fecha`, `Contenido`) VA
 -- Estructura de tabla para la tabla `servicios`
 --
 
-CREATE TABLE `servicios` (
-  `Id_serv` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `servicios` (
+  `Id_serv` int(11) NOT NULL AUTO_INCREMENT,
   `Id_admin` int(11) NOT NULL,
   `Tipo_serv` varchar(30) NOT NULL,
   `Contenido` text NOT NULL,
-  `imagen` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `imagen` varchar(50) NOT NULL,
+  PRIMARY KEY (`Id_serv`),
+  KEY `Id_admin` (`Id_admin`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `servicios`
@@ -234,120 +250,6 @@ INSERT INTO `servicios` (`Id_serv`, `Id_admin`, `Tipo_serv`, `Contenido`, `image
 (3, 3, 'Corte Textil', 'La manufactura textil es una fase de la producción económica bienes y consiste en la transformación de materias primas, en productos textiles manufacturados, productos elaborados o productos terminados para su distribución y consumo hilaturas, telas, vestido y calzado.', 'bg.jpg');
 
 --
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `administrador`
---
-ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`Id_administrador`);
-
---
--- Indices de la tabla `afiliados`
---
-ALTER TABLE `afiliados`
-  ADD PRIMARY KEY (`Id_afiliado`);
-
---
--- Indices de la tabla `aspirantes`
---
-ALTER TABLE `aspirantes`
-  ADD PRIMARY KEY (`Id_aspi`),
-  ADD KEY `Id_curs` (`Id_curs`);
-
---
--- Indices de la tabla `bolsa_formulario`
---
-ALTER TABLE `bolsa_formulario`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `id_bolsa` (`Id_bolsa`);
-
---
--- Indices de la tabla `bolsa_trabajo`
---
-ALTER TABLE `bolsa_trabajo`
-  ADD PRIMARY KEY (`Id_bolsa`),
-  ADD KEY `Id_afiliado` (`Id_afiliado`);
-
---
--- Indices de la tabla `cursos`
---
-ALTER TABLE `cursos`
-  ADD PRIMARY KEY (`Id_curs`),
-  ADD KEY `Id_admin` (`Id_admin`);
-
---
--- Indices de la tabla `maquila`
---
-ALTER TABLE `maquila`
-  ADD PRIMARY KEY (`Id_maqui`),
-  ADD KEY `Id_afiliado` (`Id_afiliado`);
-
---
--- Indices de la tabla `noticias`
---
-ALTER TABLE `noticias`
-  ADD PRIMARY KEY (`Id_not`),
-  ADD KEY `Id_admin` (`Id_admin`);
-
---
--- Indices de la tabla `servicios`
---
-ALTER TABLE `servicios`
-  ADD PRIMARY KEY (`Id_serv`),
-  ADD KEY `Id_admin` (`Id_admin`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `administrador`
---
-ALTER TABLE `administrador`
-  MODIFY `Id_administrador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `afiliados`
---
-ALTER TABLE `afiliados`
-  MODIFY `Id_afiliado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `aspirantes`
---
-ALTER TABLE `aspirantes`
-  MODIFY `Id_aspi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `bolsa_formulario`
---
-ALTER TABLE `bolsa_formulario`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `bolsa_trabajo`
---
-ALTER TABLE `bolsa_trabajo`
-  MODIFY `Id_bolsa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
---
--- AUTO_INCREMENT de la tabla `cursos`
---
-ALTER TABLE `cursos`
-  MODIFY `Id_curs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT de la tabla `maquila`
---
-ALTER TABLE `maquila`
-  MODIFY `Id_maqui` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT de la tabla `noticias`
---
-ALTER TABLE `noticias`
-  MODIFY `Id_not` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `servicios`
---
-ALTER TABLE `servicios`
-  MODIFY `Id_serv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
 -- Restricciones para tablas volcadas
 --
 
@@ -355,7 +257,7 @@ ALTER TABLE `servicios`
 -- Filtros para la tabla `bolsa_formulario`
 --
 ALTER TABLE `bolsa_formulario`
-  ADD CONSTRAINT `bolsa_formulario_ibfk_1` FOREIGN KEY (`id_bolsa`) REFERENCES `bolsa_trabajo` (`Id_bolsa`);
+  ADD CONSTRAINT `bolsa_formulario_ibfk_1` FOREIGN KEY (`Id_bolsa`) REFERENCES `bolsa_trabajo` (`Id_bolsa`);
 
 --
 -- Filtros para la tabla `bolsa_trabajo`
